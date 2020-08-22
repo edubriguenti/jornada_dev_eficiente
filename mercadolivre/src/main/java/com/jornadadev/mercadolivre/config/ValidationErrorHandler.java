22dd703fa61bf8f85eb633895c7270caa9d3df84
Eduardo Briguenti Vieira <eduardo.vieira@bestseller.com>
2020-08-22 16:00:39 +0200

    cadastro de usuario

---
+++ mercadolivre/src/main/java/com/jornadadev/mercadolivre/config/ValidationErrorHandler.java
@@ -0,0 +1,44 @@
+package com.jornadadev.mercadolivre.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ValidationErrorHandler {
+
+    private final MessageSource messageSource;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ValidationErrorsOutputDto handleValidationError(MethodArgumentNotValidException exception) {
+        final List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        final List<ObjectError> globalErrors = exception.getBindingResult().getGlobalErrors();
+        return buildValidationErrors(fieldErrors, globalErrors);
+    }
+
+    private ValidationErrorsOutputDto buildValidationErrors(List<FieldError> fieldErrors, List<ObjectError> globalErrors) {
+        ValidationErrorsOutputDto validationErrors = new ValidationErrorsOutputDto();
+        globalErrors.forEach(error ->  validationErrors.addError(getErrorMessage(error)));
+        fieldErrors.forEach(error -> {
+            String errorMessage = getErrorMessage(error);
+            validationErrors.addFieldError(error.getField(), errorMessage);
+        });
+        return validationErrors;
+    }
+
+    private String getErrorMessage(ObjectError error){
+        return messageSource.getMessage(error, LocaleContextHolder.getLocale());
+    }
+
+}
---
+++ mercadolivre/src/main/java/com/jornadadev/mercadolivre/entity/Usuario.java
@@ -0,0 +1,55 @@
+package com.jornadadev.mercadolivre.entity;
+
+import lombok.ToString;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@ToString
+public class Usuario {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Email
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    @Size(min = 6)
+    private String senha;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDateTime dataCadastro;
+
+    /**
+     * @param email string no formato de email
+     * @param senha string em texto limpo
+     */
+    public Usuario(String email, String senha) {
+        Assert.isTrue(StringUtils.hasLength(email), "Email não pode ser em branco");
+        Assert.isTrue(StringUtils.hasLength(senha), "Senha não pode ser em branco");
+        Assert.isTrue(senha.length() >= 6, "Senha deve ter no mínimo 6 caracteres.");
+        this.email = email;
+        this.senha = new BCryptPasswordEncoder().encode(senha);
+        this.dataCadastro = LocalDateTime.now();
+    }
+
+    @Deprecated
+    public Usuario(){
+
+    }
+}
